58062fcd242a690670d3cf256e658dda28f1d262
LipeMachado <silvalipe765@gmail.com>
2022-04-20 17:21:16 -0300

    Functional Game

--- script.js
+++ script.js
@@ -1,16 +1,88 @@
 window.onload = function () {
     let canvasId = document.getElementById('canvas');
-    let contexto = canvasId.getContext('2d');
+    contexto = canvasId.getContext('2d');
 
     //Variáveis
-    let snake = []
-    positionX: 10;
-    positionY: 10;
-
+    snake = [];
+    positionX = 10;
+    positionY = 10;
     foodX = 15;
     foodY = 15;
     velX = 0;
     velY = 0;
     grid = 20;
     tam = 3;
+
+    //Chamada da função jogo a cada 100 milisegundos
+    setInterval(jogo, 100)
+
+    //Controles
+    document.addEventListener("keydown", function (e) {
+        switch (e.keyCode) {
+            //Seta direita = 39
+            case 39:
+                velX = 1;
+                velY = 0;
+                break;
+            //Seta esquerda = 37
+            case 37:
+                velX = -1;
+                velY = 0;
+                break;
+            //Seta cima = 38
+            case 38:
+                velX = 0;
+                velY = -1;
+                break;
+            //Seta baixo = 40
+            case 40:
+                velX = 0;
+                velY = 1;
+                break;
+        }
+    });
+}
+
+function jogo() {
+    //Configuração da tela
+    contexto.fillStyle = "#2980B9"
+    //Distancia borda h, Distancia borda v, largura, altura
+    contexto.fillRect(0, 0, canvas.width, canvas.height)
+
+    //Deslocamento da cobra
+    positionX += velX;
+    positionY += velY;
+
+    //Espelhamento
+    if (positionX < 0) {
+        positionX = grid;
+    }
+    if (positionX > grid) {
+        positionX = 0;
+    }
+    if (positionY < 0) {
+        positionY = grid;
+    }
+    if (positionY > grid) {
+        positionY = 0;
+    }
+    
+    //Posicionando a cobra
+    snake.push({
+        x: positionX,
+        y: positionY
+    })
+
+    //Configuração da Cobra
+    contexto.fillStyle = "#00f102"
+    for (let i = 0; i < snake.length; i++) {
+        contexto.fillRect(snake[i].x*grid, snake[i].y*grid, grid-1, grid-1)
+    }
+
+    //Apagando
+    while (snake.length > tam) {
+        snake.shift()
+    }
+
+    //
 }
